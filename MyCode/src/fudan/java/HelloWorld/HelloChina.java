24dce57440ac5fe3272fcea89408e877fb974cd5
appleprince88 <huaxiajin8889@126.com>
2012-12-11 21:49:50 +0800

    china commit

--- MyCode/src/fudan/java/HelloWorld/HelloChina.java
+++ MyCode/src/fudan/java/HelloWorld/HelloChina.java
@@ -8,6 +8,9 @@ public class HelloChina {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello china");
+		System.out.println("hello china");
+		System.out.println("hello china");
+		System.out.println("hello china");
 	}
 
 }
