7d39f4412a028b49151058244d9965ae9c79dd94
appleprince88 <huaxiajin8889@126.com>
2012-12-11 21:38:42 +0800

    third commit

---
+++ MyCode/src/fudan/java/HelloWorld/HelloChina.java
@@ -0,0 +1,13 @@
+package fudan.java.HelloWorld;
+
+public class HelloChina {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello china");
+	}
+
+}
